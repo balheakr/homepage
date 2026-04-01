cf9492c0ddfa67488d765685489cda1e45bdee6c
이현호 <lhh940727@gmail.com>
2026-04-01 18:16:04 +0900

    리다이렉팅 수정3

--- js/common.js
+++ js/common.js
@@ -1,11 +1,11 @@
 // 리다이렉팅 코드
-function enforceWww() {
+(function enforceWww() {
     const { hostname, pathname, search, hash } = window.location;
 
     const protocol = location.protocol === 'http:' ? 'https:' : window.location.protocol;
   
     // 로컬 개발 환경 제외
-    if (hostname === 'localhost' || hostname === '127.0.0.1') return;
+    if (hostname === CANONICAL_HOST && protocol === 'https:') return;
   
     const CANONICAL_HOST = 'www.neobh.kr';
   
@@ -21,4 +21,4 @@ function enforceWww() {
   
     // 히스토리 남기지 않고 강제 이동
     window.location.replace(newUrl);
-};
+})();
